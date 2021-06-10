69fce21f31ece10bdbdb50396decb98b2045fa14
Willy <willyrv@torus-actions.com>
2021-06-10 10:41:54 +0200

    Modified the pre-processing script so it actually takes only 6 classes and 100 documents each class

--- 01_create_corpus.py
+++ 01_create_corpus.py
@@ -17,9 +17,9 @@ targets_to_keep = [1, 2, 7, 8, 13, 14]
 nb_of_documents_class = 100
 
 data = pd.DataFrame({"text":newsgroups.data, "target": newsgroups.target})
-corpus_5classes = data[data["target"].isin(targets_to_keep)]
-corpus_5classes_1000docs = corpus_5classes.groupby("target").sample(n=200)
-corpus_5classes_1000docs = corpus_5classes_1000docs.sample(frac=1)
+corpus_6classes = data[data["target"].isin(targets_to_keep)]
+corpus_6classes_600docs = corpus_6classes.groupby("target").head(n=100)
+#corpus_5classes_1000docs = corpus_5classes_1000docs.sample(frac=1)
 
 eng_stopwords = set(stopwords.words('english'))
 
@@ -36,7 +36,7 @@ def text2tokens(raw_text):
     # return [token for token in stemmed_tokens if len(token) > 2]  # skip short tokens
     return [token for token in tokens if len(token) > 2]  # skip short tokens
 
-dataset = [text2tokens(txt) for txt in list(corpus_5classes_1000docs['text'].values)]  # convert a documents to list of tokens
+dataset = [text2tokens(txt) for txt in list(corpus_6classes_600docs['text'].values)]  # convert a documents to list of tokens
 
 dictionary = Dictionary(documents=dataset, prune_at=None)
 dictionary.filter_extremes(no_below=5, no_above=0.3, keep_n=None)  # use Dictionary to remove un-relevant tokens
@@ -54,5 +54,5 @@ with open(PATH2DICTIONARY, 'wb') as f:
     pickle.dump(dictionary, f)
 
 # Save the text documents
-corpus_5classes_1000docs.to_csv("./20newsgroup_corpus_6classes_600docs.csv")
+corpus_6classes_600docs.to_csv("./20newsgroup_corpus_6classes_600docs.csv")
 
